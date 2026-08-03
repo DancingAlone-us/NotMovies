dc10df8dea2936585f958f15dd20055f6b017763
Drabya Hamal <hdrabya24@tbc.edu.np>
2026-08-03 09:12:16 +0545

    Added docker-compose and sql file

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE movies(
+    movie_id INTEGER PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    genres VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE links(
+    movie_id INTEGER PRIMARY KEY REFERENCES movies(movie_id),
+    imdb_id VARCHAR(20),
+    tmdb_id INTEGER
+);
+
+CREATE TABLE ratings(
+    user_id INTEGER,
+    movie_id INTEGER REFERENCES movies(movie_id),
+    rating NUMERIC(2,1),
+    timestamp BIGINT
+);
+
+CREATE TABLE tags(
+    user_id INTEGER,
+    movie_id INTEGER REFERENCES movies(movie_id),
+    tag TEXT,
+    timestamp BIGINT
+);
